6e66cf16531c4576ad7fecea2185afbcdd45e483
Richard <richard.fan2000@hotmail.com>
2026-09-06 23:55:35 -0400

    Added new packages to vim pack

---
+++ lua/kickstart/plugins/vimtex.lua
@@ -0,0 +1,5 @@
+-- To use the most recent version from git, you can do simply:
+vim.pack.add { "https://github.com/lervag/vimtex" }
+
+-- VimTeX configuration goes here, e.g.
+vim.g.vimtex_view_method = "zathura"
